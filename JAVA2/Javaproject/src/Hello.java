9854d9aeea7427b064a217d39fa9807f48821eae
MethawinNikaji <118377996+MethawinNikaji@users.noreply.github.com>
2025-08-15 13:36:38 +0700

    Add files via upload

---
+++ JAVA2/Javaproject/src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("HI This is my project");
+        final int num = 2;
+        final int num1 = 3;
+        System.out.println(num + num1 );
+    }
+
+}
